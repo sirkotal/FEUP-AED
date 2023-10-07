50f70190803bf94ae35f85d0e790be837cabf6ad
sirkotal <lordkotal2002@gmail.com>
2023-10-07 15:39:46 +0100

    Removed unnecessary folders

--- funWithCycles/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Utilizador/Desktop/CAlgorithms/funWithCycles/lib/googletest-master
-# Build directory: C:/Users/Utilizador/Desktop/CAlgorithms/funWithCycles/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- funWithCycles/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Utilizador/Desktop/CAlgorithms/funWithCycles/lib/googletest-master/googlemock
-# Build directory: C:/Users/Utilizador/Desktop/CAlgorithms/funWithCycles/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
